345029e2c8f50e4a8762d2fdae41c278bef0d690
vimal Raveendran <vimalraveendra@gmail.com>
2020-04-29 15:57:20 +0200

    created components folder and added Card Component inside also upadate the package Json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Card from "./Components/Card/Card";
 import "./App.css";
 
 class App extends React.Component {
@@ -7,6 +7,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Corona App</h1>
+        <Card />
       </div>
     );
   }
