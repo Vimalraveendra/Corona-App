c8b904bf0663772b0e580df5340a08291ee46ffe
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-03 13:46:39 +0200

    Updates

---
+++ precache-manifest.6a691ddfe10f0cb493785cfa9fb7acf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237fe0de87ee2ca880bde9d1321c51c5",
+    "url": "/Corona-App/index.html"
+  },
+  {
+    "revision": "9a35c64240c43359236e",
+    "url": "/Corona-App/static/css/main.cffbb1ce.chunk.css"
+  },
+  {
+    "revision": "5ba85e103c0dd87a40b2",
+    "url": "/Corona-App/static/js/2.a37c0c03.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-App/static/js/2.a37c0c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a35c64240c43359236e",
+    "url": "/Corona-App/static/js/main.e1996589.chunk.js"
+  },
+  {
+    "revision": "1bb40661ad96fb7815b4",
+    "url": "/Corona-App/static/js/runtime-main.af5ec259.js"
+  },
+  {
+    "revision": "04679d84118647daf5706888ebe4eb42",
+    "url": "/Corona-App/static/media/image1.04679d84.png"
+  }
+]);
